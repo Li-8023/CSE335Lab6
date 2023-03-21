a7411cf678057d7ed5719eca7a1ba0cd98614587
Li He <80284104+Li-8023@users.noreply.github.com>
2023-03-20 20:27:17 -0700

    Add files via upload

---
+++ Lab6/Lab6/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Lab6
+//
+//  Created by 贺力 on 3/20/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Lab6/Lab6/Lab6App.swift
@@ -0,0 +1,17 @@
+//
+//  Lab6App.swift
+//  Lab6
+//
+//  Created by 贺力 on 3/20/23.
+//
+
+import SwiftUI
+
+@main
+struct Lab6App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
